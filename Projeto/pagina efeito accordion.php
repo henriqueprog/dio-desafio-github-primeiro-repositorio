d62545ae9cc4488ea50c28b0f6095a250c4366cc
henriqueprog <hgbelchior@gmail.com>
2022-04-19 21:01:21 -0300

    Incluir pasta projeto com arquivos php

---
+++ Projeto/pagina efeito accordion.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="pt">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>jQuery UI Accordion - Default functionality</title>
+  <link rel="stylesheet" href "//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#accordion" ).accordion();
+  } );
+  </script>
+</head>
+<body>
+ 
+<div id="accordion">
+  <h3>Seção 1</h3>
+  <div>
+    <p>
+    Teste para o curso dio carrefour.
+    </p>
+  </div>
+  <h3>Seção 2</h3>
+  <div>
+    <p>
+    Utilizando site jQuery para realizar codigos e efeitos.
+    </p>
+  </div>
+  <h3>Seção 3</h3>
+  <div>
+    <p>
+    O efeito utilizado neste teste é o accordion.
+    </p>
+    <ul>
+      <li>List item one</li>
+      <li>List item two</li>
+      <li>List item three</li>
+    </ul>
+  </div>
+  <h3>Seção 4</h3>
+  <div>
+    <p>
+    Trabalho produzido por Henrique Belchior (henriqueprog)
+    </p>
+    <p>
+    linkedin = henrique belchior
+    </p>
+  </div>
+</div>
+ 
+ 
+</body>
+</html>
